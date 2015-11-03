9cd1316823cb6fd930c39ec5665c5c5cfb4ed5b3
Somesh Kumar <somesh.k@mobispectra.com>
2015-11-03 06:32:23 -0800

    UIKit fundamentals - Labels and Buttons

--- MemeMe/MemeMe/ViewController.swift
+++ MemeMe/MemeMe/ViewController.swift
@@ -10,14 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count = 0
+    var label:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        // Add Label
+        let label = UILabel()
+        label.frame = CGRectMake(150,150,60,60)
+        label.text = "0"
+        self.label = label
+        
+        self.view.addSubview(label)
+        
+        // Add Button
+        let button = UIButton()
+        button.frame = CGRectMake(150, 250,60,60)
+        button.setTitle("Click", forState: .Normal)
+        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        self.view.addSubview(button)
+        
+        button.addTarget(self, action: "incrementCount", forControlEvents: UIControlEvents.TouchUpInside)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func incrementCount() {
+        self.count++
+        self.label.text = "\(self.count)"
+        
+        self.view.backgroundColor = UIColor.redColor()
     }
 
 
