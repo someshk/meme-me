5f1ff88760a863726ea9818882c3c21eae81e7a8
Somesh Kumar <somesh.k@mobispectra.com>
2015-11-14 07:08:33 -0800

    Added camera and album buttons

--- MemeMe/MemeMe/ViewController.swift
+++ MemeMe/MemeMe/ViewController.swift
@@ -15,73 +15,45 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,
     var label:UILabel!
     
     @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var albumButton: UIBarButtonItem!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        
-        // Add Label
-        let label = UILabel()
-        label.frame = CGRectMake(150,150,60,60)
-        label.text = "0"
-        self.label = label
-        
-        self.view.addSubview(label)
-        
-        // Add Button
-        let button = UIButton()
-        button.frame = CGRectMake(150, 250,60,60)
-        button.setTitle("Click", forState: .Normal)
-        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
-        self.view.addSubview(button)
-        
-        button.addTarget(self, action: "incrementCount", forControlEvents: UIControlEvents.TouchUpInside)
+        cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
     }
     
     @IBAction func pickAnImage(sender: UIBarButtonItem) {
         let controller = UIImagePickerController()
+        controller.sourceType = UIImagePickerControllerSourceType.Camera
         controller.delegate = self
         self.presentViewController(controller, animated: true, completion:nil)
     }
     
     func imagePickerController(picker: UIImagePickerController,
         didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-        print("User picked an image")
-        // Close the image picker
-        self.dismissViewControllerAnimated(true, completion: nil)
+        print("imagePickerController: Image picked")
+            
+        // Close the image picker and select the image to show in the UIImageView
+        dismissViewControllerAnimated(true) { () -> Void in
+            if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+                self.imagePickerView.image = image
+            }
+        }
     }
     
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
-        print("User cancelled the operation")
-    }
-    
-    @IBAction func imageButtonClicked(sender: UIButton) {
-        let image = UIImage()
-        let controller = UIActivityViewController(activityItems: [image], applicationActivities: nil)
-        self.presentViewController(controller, animated: true, completion:nil)
+        print("imagePickerControllerDidCancel: Cancelled operation")
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
 
-    @IBAction func showAlertButtonClicker(sender: UIButton) {
-        let controller = UIAlertController()
-        let okAction = UIAlertAction (title: "Ok", style: UIAlertActionStyle.Default){ action in
-            self.dismissViewControllerAnimated(true, completion: nil)
-        }
-        let cancelAction = UIAlertAction (title: "Cancel", style: UIAlertActionStyle.Default){ action in
-            self.dismissViewControllerAnimated(true, completion: nil)
-        }
-        controller.addAction(okAction)
-        controller.addAction(cancelAction)
-        self.presentViewController(controller, animated: true, completion: nil)
+    @IBAction func albumButtonClicked(sender: UIBarButtonItem) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        imagePicker.delegate = self
+        self.presentViewController(imagePicker, animated: true, completion:nil)
     }
-    
-    func incrementCount() {
-        self.count++
-        self.label.text = "\(self.count)"
-        // Change the background color
-        self.view.backgroundColor = UIColor.redColor()
-    }
-
-
 }
 
